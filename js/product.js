dc7dc6fda21c9bcc1b802fba41dff80bbf7b6295
daoshu <daoshu>
2018-11-17 16:23:25 +0800

    更新

--- js/product.js
+++ js/product.js
@@ -7,4 +7,20 @@ $(function () {
         $('#tabBody' + index).addClass('show').removeClass('hide').siblings().addClass('hide').removeClass(
             'show');
     })
+
+    $('#tabBody4').on('click', 'li', function (ev) {
+        var src = $(this).data('src');
+        var model = $('#imgModel');
+
+        model.fadeIn();
+        var iframe = $('<iframe src="" frameborder="0" width="100%" height="100%"></iframe>');
+        model.find('.video-box').append(iframe)
+        iframe.attr('src', src)
+
+        model.find('h4').html($(this).attr('title'))
+        model.find('.glyphicon-remove').on('click', function () {
+            model.fadeOut();
+            iframe.remove();
+        })
+    })
 })
